a4b4b6bbace2ebac2c8431979c986491a6cc1f3d
Kennettechnologies <kevinonyangowanga@gmail.com>
2025-06-05 22:20:43 +0300

    Update project references to clinic_management_system in settings and wsgi

--- clinic_management/wsgi.py
+++ clinic_management/wsgi.py
@@ -1,6 +1,1 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinic_management.settings')
-
-application = get_wsgi_application() 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinic_management_system.settings') 
